2c59e23b4f8157de5c6d26a9e54c6271842536bf
vctr-x <71457380+vctr-x@users.noreply.github.com>
2020-12-09 12:39:29 -0500

    Add files via upload

---
+++ temadesc14.php
@@ -0,0 +1,16 @@
+<?php
+    $conectar = include 'Conexion.php';
+
+    $sqlT0001= "SELECT * FROM tbl_temas WHERE ID_Tema = 'TNB_0014'";
+    
+    $ejecutarT0001= mysqli_query($conectar, $sqlT0001);
+
+    if(!$ejecutarT0001){
+        header("location:Error.html");
+    }else{
+        while($rowT0001 = $ejecutarT0001->fetch_array()){
+            $T0001Desc = $rowT0001['DescrTema'];
+            return $T0001Desc;
+        }
+    }
+?>
